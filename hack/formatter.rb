fc7b208e264128b16693556c2bf077154ebf36aa
Gregory Brown <gregory.t.brown@gmail.com>
2011-10-25 00:38:37 -0400

    Matching sample text now, mostly

--- hack/formatter.rb
+++ hack/formatter.rb
@@ -4,14 +4,22 @@ data = JSON.parse(File.read(ARGV[0]))
 
 color_to_symbol = { "black" => "B", "white" => "W" }
 
-output = data.map do |row|
-  row.map do |color, strength|
+header    = "       0      1      2      3      4      5      6      7      8\n"
+separator = "       |      |      |      |      |      |      |      |      |\n"
+
+border_b  = "     BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB\n"
+border_w  = "     WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW\n"
+
+battlefield_text = data.map.with_index do |row, row_index|
+  row_text = row.map do |color, strength|
     if color == "unclaimed"
       "(___)"
     else
       "(#{color_to_symbol[color]}#{strength.to_s.rjust(2)})"
     end
-  end.join("--") + "\n"
-end.join("  |      |      |      |      |      |      |      |      |\n")
+  end.join("--")
 
-puts output
+  "#{row_index.to_s.rjust(3)}  #{row_text}\n"
+end.join(separator)
+
+puts [header, border_b, battlefield_text, border_w].join
