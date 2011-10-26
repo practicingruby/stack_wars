5211eeffec6989a6f5312ba04cb5f86bb313976b
Gregory Brown <gregory.t.brown@gmail.com>
2011-10-26 16:24:07 -0400

    simplify sample game using TextClient

--- example/sample_game.rb
+++ example/sample_game.rb
@@ -4,6 +4,8 @@ field = StackWars::Battlefield.new
 game  = StackWars::Game.new(field)
 moves = File.foreach("#{File.dirname(__FILE__)}/sample-moves.txt")
 
+client = StackWars::TextClient.new(game)
+
 message = catch(:game_over) do
   loop do
     system "clear"
@@ -15,16 +17,10 @@ message = catch(:game_over) do
          "#{game.opponent.color}: "+
          "#{game.opponent.successful_invasions}"
 
-    input = moves.next
-    print "#{game.active_player.color}:#{game.active_player.reserves} > #{input}"
+    command = moves.next
+    print "#{game.active_player.color}:#{game.active_player.reserves} > #{command}"
+    client.play(command)
 
-    md = input.match(/^ *(?<pos1>\d+ +\d+) *(?<pos2>\d+ +\d+)? *$/)
-
-    if md[:pos2]
-      game.play(md[:pos1].scan(/\d+/).map(&:to_i), md[:pos2].scan(/\d+/).map(&:to_i))
-    else
-      game.play(md[:pos1].scan(/\d+/).map(&:to_i))
-    end
 
     puts "hit enter to continue"
     gets
